b617a45c61a342823f03a6869dc69fccc0455f9f
Milena A Kosc <mak68@afsconnect1.njit.edu>
2017-12-13 20:21:51 -0500

    created new html for creating task

---
+++ pages/create_task.php
@@ -0,0 +1,32 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+
+<form action="index.php?page=tasks&action=register" method="post">
+    Create Date: <input type="text" name="createdate"><br>
+    Due Date: <input type="text" name="duedate"><br>
+    Message: <input type="text" name="message"><br>
+    Done?: <input type="text" name="isdone"><br>
+    <input type="submit" value="Submit form">
+</form>
+
+
+<script src="js/scripts.js"></script>
+</body>
+</html>
