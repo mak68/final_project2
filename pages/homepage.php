f6e7894770eb93864ba1648cd836a98963e2ba8e
Milena A Kosc <mak68@afsconnect1.njit.edu>
2017-12-17 13:28:49 -0500

    added front end design test

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,50 +1,113 @@
-<!doctype html>
-
-<html lang="en">
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6 lt8"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7 lt8"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8 lt8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8" />
+    <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">  -->
+    <title>Login and Registration Form with HTML5 and CSS3</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Login and Registration Form with HTML5 and CSS3" />
+    <meta name="keywords" content="html5, css3, form, switch, animation, :target, pseudo-class" />
+    <meta name="author" content="Codrops" />
 
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
-
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
-
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <![endif]-->
+    <link rel="stylesheet" type="text/css" href="pages/assets/css/demo.css" />
+    <link rel="stylesheet" type="text/css" href="pages/assets/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="pages/assets/css/animate-custom.css" />
 </head>
-
 <body>
+<div class="container">
+    <!-- Codrops top bar -->
+    <div class="codrops-top">
 
 
-<h1>
-    <?php
+        <div class="clr"></div>
+    </div><!--/ Codrops top bar -->
+    <header>
+        <h1>To Do Application</h1>
+        <nav class="codrops-demos">
 
-    //this how to print some data;
-    echo $data['site_name'];
+        </nav>
+    </header>
+    <section>
+        <div id="container_demo" >
+            <!-- hidden anchor to stop jump http://www.css3create.com/Astuce-Empecher-le-scroll-avec-l-utilisation-de-target#wrap4  -->
+            <a class="hiddenanchor" id="toregister"></a>
+            <a class="hiddenanchor" id="tologin"></a>
+            <div id="wrapper">
+                <div id="login" class="animate form">
+                    <form  action="index.php?page=accounts&action=login" method="post" autocomplete="on">
+                        <h1>Log in</h1>
+                        <p>
+                            <label for="username" class="uname" data-icon="u" > Your email or username </label>
+                            <input id="username" name="email" required="required" type="text" placeholder=""/>
+                        </p>
+                        <p>
+                            <label for="password" class="youpasswd" data-icon="p"> Your password </label>
+                            <input id="password" name="password" required="required" type="password" placeholder="" />
+                        </p>
+                        <p class="keeplogin">
+                            <input type="checkbox" name="loginkeeping" id="loginkeeping" value="loginkeeping" />
+                            <label for="loginkeeping">Keep me logged in</label>
+                        </p>
+                        <p class="login button">
+                            <input type="submit" value="Login" />
+                        </p>
+                        <p class="change_link">
+                            Not a member yet ?
+                            <a href="#toregister" class="to_register">Sign Up</a>
+                        </p>
+                    </form>
+                </div>
 
-    ?> </h1>
+                <div id="register" class="animate form">
+                    <form  action="index.php?page=accounts&action=register" method="post" autocomplete="on">
+                        <h1> Sign up </h1>
+                        <p>
+                            <label for="usernamesignup" class="uname" data-icon="u">First Name</label>
+                            <input id="usernamesignup" name="fname" required="required" type="text" placeholder="" />
+                        </p>
+                        <p>
+                            <label for="usernamesignup" class="uname" data-icon="u">Last Name</label>
+                            <input id="usernamesignup" name="lname" required="required" type="text" placeholder="" />
+                        </p>
+                        <p>
+                            <label for="emailsignup" class="youmail" data-icon="e" > Email</label>
+                            <input id="emailsignup" name="email" required="required" type="email" placeholder=""/>
+                        </p>
+                        <p>
+                            <label for="emailsignup" class="youmail" data-icon="u" > Phone</label>
+                            <input id="emailsignup" name="Phone" required="required" type="text" placeholder=""/>
+                        </p>
+                        <p>
+                            <label for="emailsignup" class="uname" data-icon="u" > Birthday</label>
+                            <input id="emailsignup" name="birthday" required="required" type="text" placeholder=""/>
+                        </p>
+                        <p>
+                            <label for="emailsignup" class="uname" data-icon="u" > Gender</label>
+                            <input id="emailsignup" name="gender" required="required" type="text" placeholder=""/>
+                        </p>
+                        <p>
+                            <label for="password" class="youpasswd" data-icon="p"> Your password </label>
+                            <input id="password" name="password" required="required" type="password" placeholder="" />
+                        </p>
 
 
+                        <p class="signin button">
+                            <input type="submit" value="Sign up"/>
+                        </p>
+                        <p class="change_link">
+                            Already a member ?
+                            <a href="#tologin" class="to_register"> Log in </a>
+                        </p>
+                    </form>
+                </div>
 
-<form action="index.php?page=accounts&action=login" method="POST">
-
-    <div class="container">
-        <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="email" required>
-
-        <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
-
-        <button type="submit">Login</button>
-    </div>
-
-
-</form>
-<h1><a href="index.php?page=accounts&action=register">Register</a></h1>
-
-
-<script src="js/scripts.js"></script>
+            </div>
+        </div>
+    </section>
+</div>
 </body>
 </html>
